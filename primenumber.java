68aeeee6ac776b22c29054157d2c5733c70a14cc
Velkumar V <104992954+velkumarv@users.noreply.github.com>
2025-04-05 20:37:27 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -6,7 +6,7 @@ public static void main(String[] args) {
 			int count=0;																																												
 			for (int i = 1; i <= A ; i++) {
 				if (A%i==0) {
-				
+				count++;
 				}
 			}
 			if(count==2) {
